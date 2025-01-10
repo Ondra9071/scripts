21c467c768a6d4476fc0198708bd34a5e0f0731a
Ondřej Pacovský <ondra907@email.cz>
2025-01-10 22:37:27 +0100

    Add files via upload

---
+++ docker-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo 'Installing docker...'
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+    sudo apt-get remove -y $pkg
+done
+sudo rm -f /etc/apt/sources.list.d/docker.list
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
+
+echo 'Docker installed.'
